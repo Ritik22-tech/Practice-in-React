bffd626afb67b830834f9c84580e1b99283ed8b3
Ritik Toppo <107831105+Ritik22-tech@users.noreply.github.com>
2022-10-01 23:48:14 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,18 +3,37 @@ import './App.css';
 import About from './Component/About';
 import Navbar from './Component/Navbar';
 import Textform from './Component/Textform';
+import React, { useState } from 'react';
 
-function App() {
+function App(props) {
+
+  const [mode, setMode] = useState('light');
+  const [modeType, setModeType] = useState('Enable Dark Mode')
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      setModeType('Enable Light Mode')
+      document.body.style.backgroundColor = 'rgb(64, 64, 64)'
+    }
+    else {
+      setMode('light')
+      setModeType('Enable Dark Mode')
+      document.body.style.backgroundColor = 'white'
+    }
+  }
+
   return (
     <>
-      <Navbar title="TextUtils" home="Home" about="About_Us" />
+      <Navbar title="TextUtils" home="Home" about="About_Us" mode={mode} toggleMode={toggleMode} Type={modeType} />
       <div className="container">
-        <Textform heading="Enter your Text below to analyse" heading2="Analysation" heading3="Preview"/>
+        <Textform heading="Enter your Text below to analyse" heading2="Analysation" heading3="Preview" mode={mode} />
       </div>
       <hr />
       <div className="container">
         <About />
       </div>
+
     </>
   );
 }
--- src/Component/Textform.js
+++ src/Component/Textform.js
@@ -26,7 +26,7 @@ export default function Textform(props) {
 
     const CountText = () => {
         let count = text.split(" ").length
-        return count-1
+        return count - 1
     }
 
     const HandleExtraSpaces = () => {
@@ -38,43 +38,45 @@ export default function Textform(props) {
         setText("")
     }
 
-    const Font=()=>{
+    const Font = () => {
         let ChangeFont = document.getElementById('mybox');
-        ChangeFont.style.fontFamily = "Italic"
-        // console.log("clicked")
-        // if (ChangeFont.style.fontFamily == "none") {
-        //     ChangeFont.style.fontFamily = "monospace"
-        // }
+        if (ChangeFont.style.fontFamily === "none") {
+            ChangeFont.style.fontFamily = "monospace"
+        }
+        else {
+            ChangeFont.style.fontFamily = "none"
+        }
     }
 
     const [text, setText] = useState('');
 
     return (<>
-        <div className="my-3">
-            <h4 >{props.heading}</h4>
+        <div className={`my-3 text-${props.mode === 'dark' ? 'light' : 'dark'}`}>
+            <h4>{props.heading}</h4>
         </div>
         <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={HandleOnChange} id="mybox" placeholder='Write Here' rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={HandleOnChange} style={{color: props.mode === 'dark' ? 'white' : 'black',backgroundColor: props.mode === 'light' ? 'white' : 'rgb(64, 64, 64)', border: props.mode === 'light' ? '' : '3px Solid blue' }} id="mybox" placeholder='Write Here' rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mx-2" onClick={HandleUpClick}>Convert to Uppercase</button>
-        <button className="btn btn-primary mx-2" onClick={HandleLoClick}>Convert to Lowercase</button>
-        <button className="btn btn-primary mx-2" onClick={Copybtn}>To Copy</button>
-        <button className="btn btn-primary mx-2" onClick={HandleExtraSpaces}>Remove Extra Spaces</button>
-        <button className="btn btn-primary mx-2" onClick={ClearAll}>Clear All</button>
-        <button className="btn btn-primary mx-2" onClick={Font}>Change Font</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={HandleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={HandleLoClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={Copybtn}>To Copy</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={HandleExtraSpaces}>Remove Extra Spaces</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={ClearAll}>Clear All</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={Font}>Change Font</button>
 
-        <div className="my-4">
-            <h5>{props.heading2}</h5>
-        </div>
-        {/* <p> {text.split(" ").length-1} Words and {text.length} Characters in Text box</p> */}
-        <p> {CountText()} Words and {text.length} Characters in Text box</p>
-        <p>{0.008 * text.split(" ").length} Minutes to read this Textbox</p>
+        <div style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+            <div className="my-4">
+                <h5>{props.heading2}</h5>
+            </div>
+            {/* <p> {text.split(" ").length-1} Words and {text.length} Characters in Text box</p> */}
+            <p> {CountText()} Words and {text.length} Characters in Text box</p>
+            <p>{0.008 * text.split(" ").length} Minutes to read this Textbox</p>
 
-        <div className="my-4">
-            <h5>{props.heading3}</h5>
+            <div className="my-4">
+                <h5>{props.heading3}</h5>
+            </div>
+            <p>{text.length>0?text:"Enter Something To Preview"}</p>
         </div>
-        <p>{text}</p>
-        
     </>
     )
 }
