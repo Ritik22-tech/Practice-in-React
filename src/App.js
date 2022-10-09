a749078cbbc0ee22b198b3f1de5f653768d59c22
Ritik Toppo <107831105+Ritik22-tech@users.noreply.github.com>
2022-10-10 00:24:12 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,39 +1,62 @@
-// import logo from './logo.svg';
 import './App.css';
 import About from './Component/About';
 import Navbar from './Component/Navbar';
 import Textform from './Component/Textform';
+import Alert from './Component/Alert';
 import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+// Writing Functional Logics
 
 function App(props) {
 
   const [mode, setMode] = useState('light');
   const [modeType, setModeType] = useState('Enable Dark Mode')
+  const [alert, setAlert] = useState(null)
 
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark')
       setModeType('Enable Light Mode')
       document.body.style.backgroundColor = 'rgb(64, 64, 64)'
+      showAlert("Dark Mode is Enabled !!", "success")
+
     }
     else {
       setMode('light')
       setModeType('Enable Dark Mode')
       document.body.style.backgroundColor = 'white'
+      showAlert("Light Mode is Enabled !!", "success")
     }
+  }
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
   }
 
   return (
     <>
+      <Router>
       <Navbar title="TextUtils" home="Home" about="About_Us" mode={mode} toggleMode={toggleMode} Type={modeType} />
+      <Alert alert={alert} />
       <div className="container">
-        <Textform heading="Enter your Text below to analyse" heading2="Analysation" heading3="Preview" mode={mode} />
+        <Routes>
+        <Route exact path="/" element={<Textform heading="Enter your Text below to analyse" heading2="Analysation" heading3="Preview" mode={mode} showAlert={showAlert} />}></Route>
+        {/* <Textform heading="Enter your Text below to analyse" heading2="Analysation" heading3="Preview" mode={mode} showAlert={showAlert} /> */}
+        <Route exact path="/about" element={<About />}></Route>
+        </Routes>
       </div>
-      <hr />
-      <div className="container">
-        <About />
-      </div>
-
+      </Router>
     </>
   );
 }
--- src/Component/Textform.js
+++ src/Component/Textform.js
@@ -5,18 +5,21 @@ export default function Textform(props) {
         console.log("Convert to Uppercase Clicked")
         let upcase = text.toUpperCase()
         setText(upcase)
+        props.showAlert("Converted To Uppercase !!", "success")
     }
 
     const HandleLoClick = () => {
         console.log("Convert to Lowercase Clicked")
         let locase = text.toLowerCase()
         setText(locase)
+        props.showAlert("Converted To Lowercase !!", "success")
     }
 
     const Copybtn = () => {
         let cpy = document.getElementById('mybox')
         cpy.select()
         navigator.clipboard.writeText(cpy.value)
+        props.showAlert("Text is Copied Successfully !!", "success")
     }
 
     const HandleOnChange = (event) => {
@@ -24,18 +27,15 @@ export default function Textform(props) {
         setText(event.target.value)
     }
 
-    const CountText = () => {
-        let count = text.split(" ").length
-        return count - 1
-    }
-
     const HandleExtraSpaces = () => {
-        let newText = text.split(/[ ]+/)
+        let newText = text.split(/[ ]+/) 
         setText(newText.join(" "))
+        props.showAlert("Extra Spaces Are Cleared !!", "success")
     }
 
     const ClearAll = () => {
         setText("")
+        props.showAlert("Text Area is Cleared !!", "success")
     }
 
     const Font = () => {
@@ -48,6 +48,29 @@ export default function Textform(props) {
         }
     }
 
+    // const CountText = (content) => {
+    //     if (content === "") {
+    //         return 0;
+    //     }
+    //     let arr = content.split(" ");
+    //     let count = 0;
+
+    //     for (let i = 0; i < arr.length; i++) {
+    //         if (arr[i] === '' || arr[i] === ' ') {
+    //             count++;
+    //         }
+    //     }
+    //     return arr.length - count;   
+    // }
+
+    // const countWord = () => {
+    //     if (text.length > 0) {
+    //         return text.trim().split(" ").length;
+    //     }
+    //     else {
+    //         return 0;
+    //     }
+    // }
     const [text, setText] = useState('');
 
     return (<>
@@ -55,7 +78,7 @@ export default function Textform(props) {
             <h4>{props.heading}</h4>
         </div>
         <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={HandleOnChange} style={{color: props.mode === 'dark' ? 'white' : 'black',backgroundColor: props.mode === 'light' ? 'white' : 'rgb(64, 64, 64)', border: props.mode === 'light' ? '' : '3px Solid blue' }} id="mybox" placeholder='Write Here' rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={HandleOnChange} style={{ color: props.mode === 'dark' ? 'white' : 'black', backgroundColor: props.mode === 'light' ? 'white' : 'rgb(64, 64, 64)', border: props.mode === 'light' ? '' : '3px Solid blue' }} id="mybox" placeholder='Write Here' rows="8"></textarea>
         </div>
         <button className="btn btn-primary mx-2 my-2" onClick={HandleUpClick}>Convert to Uppercase</button>
         <button className="btn btn-primary mx-2 my-2" onClick={HandleLoClick}>Convert to Lowercase</button>
@@ -68,14 +91,15 @@ export default function Textform(props) {
             <div className="my-4">
                 <h5>{props.heading2}</h5>
             </div>
-            {/* <p> {text.split(" ").length-1} Words and {text.length} Characters in Text box</p> */}
-            <p> {CountText()} Words and {text.length} Characters in Text box</p>
+            {/* <p> {countWord()} Words and {text.length} Characters in Text box</p>
+            <p> {CountText(text)} Words and {text.length} Characters in Text box</p> */}
+            <p> {text.split(" ").filter((element)=>{return element.length!==0}).length} Words and {text.length} Characters in Text box</p>
             <p>{0.008 * text.split(" ").length} Minutes to read this Textbox</p>
 
             <div className="my-4">
                 <h5>{props.heading3}</h5>
             </div>
-            <p>{text.length>0?text:"Enter Something To Preview"}</p>
+            <p>{text.length > 0 ? text : "Enter Something To Preview"}</p>
         </div>
     </>
     )
